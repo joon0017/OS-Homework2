07e4ad3c874605c5e63ebccda2344a3d3c8ccac0
joon0017 <97450065+joon0017@users.noreply.github.com>
2023-04-05 04:33:19 +0900

    Added ExecutePrgm and update on Reduce

--- cimin.c
+++ cimin.c
@@ -12,34 +12,70 @@
 //two pipes needed => pipe[0]:standard input, pipe[1]:standard output
 int pipes[2];
 
-void ExecutePrgm()
+void ExecutePrgm(char* t, char* prgm){
+    pid_t child=fork();
+    if(child<0) {
+        perror("Error(fork)");
+        exit(1);
+    } else if (child == 0){
+        //child process
+        close(pipes[0]);	//close reading pipe
+        dup2(pipes[1],1);	//redirect stdout to writing pipe
+        close(pipes[1]);	//close writing pipe
+        execvp(prgm,t); //run program
+        exit(33);
+    } else{
+        //parent process
+        int status;
+        wait(&status);
+        if(WEXITSTATUS(status) == 33){
+            //found the error string (crashes)
+            return true;
+        }
+        else return false;
+    }
+}
 
-char* Reduce(char* t){
+char* Reduce(char* t, char* prgm){
     int s = strlen(t) - 1;
     char* head;
     char* mid;
     char* tail;
 
     while(s > 0){
-        for(int i = 0; i < strlen(t) - s ;i++){
+        for(int i = 0; i < strlen(t) - s - 1;i++){
             //allocate memory for each part
             head = (char*)malloc(sizeof(char)*i+1);
-            mid = (char*)malloc(sizeof(char)*s+1);
             tail = (char*)malloc(sizeof(char)*(strlen(t)-i-s)+1);
-
+            
             //copy the substrings to each part
             strncpy(head,t,i);
-            strncpy(mid,t+i,s);
             strncpy(tail,t+i+s,strlen(t)-i-s);
 
             //foolproof method to make sure the strings are null terminated
             head[i] = '\0';
-            mid[s] = '\0';
             tail[strlen(t)-i-s] = '\0';
 
+            strcat(head,tail);	//concatenate head and tail
+            
+            //if the string with the problem is found, reduce it further
+            //(crashes)
+            if(ExecutePrgm(head,prgm)) return Reduce(head,prgm);	
+
+        }
+        for(int i = 0; i < strlen(t) - s - 1 ;i++){
+            //find middle part
+            mid = (char*)malloc(sizeof(char)*s+1);
+            strncpy(mid,t+i,s);
+            mid[s] = '\0';
+
+            //try to find the error string in the middle part
+            if(ExecutePrgm(mid,prgm)) return Reduce(mid,prgm);
         }
         s--;
     }
+
+    return t;
 }
 
 void InputAnalysis(int argc, int paramlen, char* argv[], char* returnArr[4], char* tarArg[paramlen]) {
@@ -104,7 +140,7 @@ void terminate(int sig) { //when SIGINT(control+c) happens
 }
 
 int main(int argc, char* argv[]) {
-    pid_t child_pid;
+    // pid_t child_pid;
 
     signal(SIGINT,terminate);	//when SIGINT, calls terminate to exit
     signal(SIGALRM,timeout);	//when SIGALRM, calls timeout to exit
@@ -127,7 +163,7 @@ int main(int argc, char* argv[]) {
     for(int i=0;i<paramlen;i++){
     	printf("%s, ",tarArg[i]);
     }
-    printf("%s\n",targetArg[0]);
+    printf("\n");
 
     printf("Input path: %s\nError String: %s\nOutput path: %s\nProgram to be executed: %s\n", inputs[0], inputs[1], inputs[2], inputs[3]);
      
@@ -158,33 +194,35 @@ int main(int argc, char* argv[]) {
 
     ssize_t s;
     char buf[1024];
-   
-    child_pid=fork();
-    if(child_pid<0){
-    	perror("Fork Error");
-        exit(1);
-    }
-    if(child_pid==0){  			//child process
-	
-        close(pipes[0]);                //close reading pipe
-    	dup2(pipes[1],STDERR_FILENO); 	//standard error -> writing pipe
 
-        execvp(inputs[3],tarArg);	//executing jsondump in child process
-        perror("execl failed");
-        exit(1);
-    }
-    else{				//parent process
-    	wait(0x0);			//wait until the child process is done
-    	close(pipes[1]); 		//close writing pipe
+    Reduce(crashInput,inputs[3]);
+   
+    // child_pid=fork();
+    // if(child_pid<0){
+    // 	perror("Fork Error");
+    //     exit(1);
+    // }
+    // if(child_pid==0){  			//child process
 	
-    	while(1){
-    	    s=read(pipes[0],buf,1023);
-	    buf[s+1]=0x0;
-    	    printf("stderr> %s\n",buf);
-	    sleep(5);
-	//write(pipes[1],input,4096);
-	}
-    }
+    //     close(pipes[0]);                //close reading pipe
+    // 	dup2(pipes[1],STDERR_FILENO); 	//standard error -> writing pipe
+
+    //     execvp(inputs[3],tarArg);	//executing jsondump in child process
+    //     perror("execl failed");
+    //     exit(1);
+    // }
+    // else{				//parent process
+    // 	wait(0x0);			//wait until the child process is done
+    // 	close(pipes[1]); 		//close writing pipe
+	
+    // 	while(1){
+    // 	    s=read(pipes[0],buf,1023);
+	//     buf[s+1]=0x0;
+    // 	    printf("stderr> %s\n",buf);
+	//     sleep(5);
+	// //write(pipes[1],input,4096);
+	// }
+    // }
 
     return 0;
 
