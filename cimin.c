640c0b1b0f657b36f9ae6343a5707ce9a22842e8
Sungmin Kim <glass@handong.ac.kr>
2023-04-05 22:57:49 +0900

    working on ExecutePrgm & Reduce

--- cimin.c
+++ cimin.c
@@ -14,20 +14,46 @@ int pipes[2];
 int crin[2];
 //pipe[0]:standard input, pipe[1]:standard output
 
-bool ExecutePrgm(char* t, char* prgm){
+bool ExecutePrgm(char* crash, char* prgm, char* param){
+    ssize_t s;
+    char buf[1024];
+
     pid_t child=fork();
     if(child<0) {
         perror("Error(fork)");
         exit(1);
-    } else if (child == 0){
-        //child process
+    }
+    else if (child == 0){	 //child process
+	/*
         close(pipes[0]);	//close reading pipe
         dup2(pipes[1],1);	//redirect stdout to writing pipe
         close(pipes[1]);	//close writing pipe
         execvp(prgm,t); //run program
         exit(33);
-    } else{
-        //parent process
+	*/
+	close(crin[1]);			//close writing pipe so it can read crashing input form pipe
+	dup2(crin[0],STDIN_FILENO);	//standard input -> reading pipe
+	
+	execv(/*inputs[3], tarArg*/prgm,param);	//executing jsondump in child process
+	perror("execvp failed");
+	exit(/*1*/33);
+    }
+    else{		//parent process
+	close(pipes[1]); 		//close writing pipe
+	close(crin[0]);			//close reading pipe to write crashing input to pipe
+
+	int check=write(crin[1]/*,crashInput,crlen+1*/,crash,strlen(crash)+1);	//write crashing input to writing pipe of crin
+	if(check!=strlen(crash)+1/*crlen+1*/) perror("write malfunctioning");
+	close(crin[1]);				//since writing to crin is done, close writing pipe
+	
+	int sum=0;
+    	while(sum<check){
+    	    s=read(pipes[0],buf,1023);
+	    sum+=s;
+	    buf[s+1]=0x0;
+    	    printf("stderr> %s",buf);
+	}	
+
         int status;
         wait(&status);
         if(WEXITSTATUS(status) == 33){
@@ -38,33 +64,33 @@ bool ExecutePrgm(char* t, char* prgm){
     }
 }
 
-char* Reduce(char* t, char* prgm){
-    int s = strlen(t) - 1;
+char* Reduce(char* crash, char* prgm, char* param){
+    int s = strlen(crash) - 1;
     char* head;
     char* mid;
     char* tail;
 
     while(s > 0){
-        for(int i = 0; i < strlen(t) - s - 1;i++){
+        for(int i = 0; i < strlen(crash) - s - 1;i++){
             //allocate memory for each part
             head = (char*)malloc(sizeof(char)*i+1);
-            tail = (char*)malloc(sizeof(char)*(strlen(t)-i-s)+1);
+            tail = (char*)malloc(sizeof(char)*(strlen(crash)-i-s)+1);
             
             //copy the substrings to each part
-            strncpy(head,t,i);
-            strncpy(tail,t+i+s,strlen(t)-i-s);
+            strncpy(head,crash,i);
+            strncpy(tail,crash+i+s,strlen(crash)-i-s);
 
             //foolproof method to make sure the strings are null terminated
             head[i] = '\0';
-            tail[strlen(t)-i-s] = '\0';
+            tail[strlen(crash)-i-s] = '\0';
 
             strcat(head,tail);	//concatenate head and tail
             
             //if the string with the problem is found, reduce it further
             //(crashes)
-            if(ExecutePrgm(head,prgm)) {
+            if(ExecutePrgm(head,prgm,param)) {
                 printf("\nfound error string: %s...\nNow reducing further",head);
-                return Reduce(head,prgm);	
+                return Reduce(head,prgm,param);	
             }
 
         }
@@ -78,13 +104,13 @@ char* Reduce(char* t, char* prgm){
             if(ExecutePrgm(mid,prgm)) 
             {
                 printf("\nfound error string: %s...\nNow reducing further",mid);
-                return Reduce(mid,prgm);
+                return Reduce(mid,prgm,param);
             }
         }
         s--;
     }
-    printf("\nFinished reducing. Error string is: %s\n",t);
-    return t;
+    printf("\nFinished reducing. Error string is: %s\n",mid);
+    return mid;
 }
 
 void InputAnalysis(int argc, int paramlen, char* argv[], char* returnArr[4], char* tarArg[paramlen]) {
@@ -207,6 +233,8 @@ int main(int argc, char* argv[]) {
 
     printf("check if input is printed\n");
 
+    //implemented in Execute_Prgm
+    /*
     ssize_t s;
     char buf[1024];
 
@@ -241,7 +269,8 @@ int main(int argc, char* argv[]) {
 
 	wait(0x0);			//wait until the child process is done
     }
-
+    */
+    Reduce(crashInput,inputs[3],tarArg);
    
     return 0;
 
